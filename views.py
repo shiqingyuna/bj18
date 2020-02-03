78df7ee967d4a90c1b319814e0f2ceea10bf3b73
shiqingyuna <453484110@qq.com>
2020-02-03 14:03:59 +0800

    创建视图

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.http import Httpresponse
-
+from django.shortcuts import redirect
 def index(request):
     return HttpResponse('index')
 
+def login(request):
+    return redirect('index')
